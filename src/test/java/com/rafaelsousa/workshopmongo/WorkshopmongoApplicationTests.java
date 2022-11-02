b90a6f05ed47115a0761a31f4971dcc273246928
Rafael Sousa <rafiinhaa.sousa@hotmail.com>
2022-11-02 11:37:05 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/rafaelsousa/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelsousa.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
