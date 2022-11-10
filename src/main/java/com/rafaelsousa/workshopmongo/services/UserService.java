f8cd686183f8d86e02de5881024fdbeb7d5a4a2f
Rafael Sousa <rafiinhaa.sousa@hotmail.com>
2022-11-10 13:04:44 -0300

    Inserção de usuário com POST

--- src/main/java/com/rafaelsousa/workshopmongo/services/UserService.java
+++ src/main/java/com/rafaelsousa/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rafaelsousa.workshopmongo.domain.User;
+import com.rafaelsousa.workshopmongo.dto.UserDTO;
 import com.rafaelsousa.workshopmongo.repository.UserRepository;
 import com.rafaelsousa.workshopmongo.services.esception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 	
 }
